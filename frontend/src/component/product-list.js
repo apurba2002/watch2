5a48ae30b5501c9766f68a8f36fa1c8bf3e2d6f8
apurba2002 <debnathapurba582@gmail.com>
2023-09-12 15:09:53 +0600

    do something

--- frontend/src/component/product-list.js
+++ frontend/src/component/product-list.js
@@ -5,7 +5,7 @@ function Product_list(props) {
       <div className="product">
         <p>you may also be into</p>
         <div className="card-grp">
-          <div className="card">
+          <div className="card" onClick={()=>{window.location.href="/p2"}}>
             <img src={props.product_1_img} alt="" />
             <p>{props.product_1_tittle_1}</p>
             <p>{props.product_1_tittle_2}</p>
@@ -23,7 +23,7 @@ function Product_list(props) {
             <p>{props.product_3_tittle_2}</p>
             <p>{props.product_3_price}</p>
           </div>
-          <div className="card">
+          <div className="card" onClick={()=>{window.location.href="/"}}>
             <img src={props.product_4_img} alt="" />
             <p>{props.product_4_tittle_1}</p>
             <p>{props.product_4_tittle_2}</p>
